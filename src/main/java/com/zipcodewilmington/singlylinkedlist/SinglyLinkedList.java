4ceabd46da7b7bae9b8a77fa88e45dd3e268bac4
Carnell <92267723+c-poteat@users.noreply.github.com>
2021-12-06 16:12:29 -0500

    Worked on as a group

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,7 +3,7 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList<T> {
+public class SinglyLinkedList<T extends Comparable> {
 
     private Node<T> head;
     private Node<T> tail;
@@ -23,123 +23,156 @@ public class SinglyLinkedList<T> {
         public Node(T data) {
             this.data = data;
         }
+    }
 
-        public void add(T data) {
-            //Create a new node
-            Node newNode = new Node(data);
-            if (head == null) {
-                //If list is empty, both head and tail will point to new node
-                head = newNode;
-                tail = newNode;
+    public void add(T data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void remove(int index) {
+        //given the index to remove
+        //iterate through the list
+        Node<T> currentNode = head, previousNode = null;
+        int currentIndex = 0;
+        //check if the current node exist
+        while (currentNode != null && currentIndex != index) {
+            //keeping track of the previous node
+            previousNode = currentNode;
+            //continuing the iteration
+            currentNode = currentNode.next;
+            //iterate through the next node
+            currentIndex++;
+        }
+        //remove the index
+        if (currentIndex == index) {
+            //checking if the previous node is not null
+            if (previousNode != null) {
+                //then we can add the previous node to the node after
+                previousNode.next = currentNode.next;
+            }
+            //checking that the current node is the head
+            if (currentNode == head) {
+                //and also if the current node is null
+                if (currentNode == null || currentNode.next == null) {
+                    //return head as null;
+                    head = null;
+                } else {
+                    //the new head will be the next node
+                    head = currentNode.next;
+                }
+                //current is the tail
+            } else if (currentNode == tail) {
+                //the tail is the previous node
+                tail = previousNode;
+            }
+
+        }
+
+    }
+
+    public Boolean contains(T element) {
+        //given a T element
+        //loop through the list starting from the head
+        //each time check if it contains the element
+        Node<T> currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.data != element) {
+                currentNode = currentNode.next;
             } else {
-                //newNode will be added after tail such that tail's next will point to newNode
-                tail.next = newNode;
-                // newNode will become new tail of the list
-                tail = newNode;
+                return true;
             }
         }
+        return false;
 
-        public void remove(int index) {
-            //iterate through the singlyList
-            Node currentNode = head, previousNode = null;
-            int currentIndex = 0;
-            //check if the current node exist
-            while (currentNode != null && currentIndex != index) {
-                previousNode = currentNode;
-                // continuing the iteration
+    }
+
+    public int find(T element) {
+        //create indexCounter
+        //iterate through the node
+        //when you find the element
+        //return the index where element is found
+        int currentIndex = 0;
+        Node<T> currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.data != element) {
                 currentNode = currentNode.next;
                 currentIndex++;
             }
-            // remove the index
-            if (currentIndex == index) {
-                //checking of the previous node is not null
-                if (previousNode != null) {
-                    // pointing the previous node to the node after
-                    previousNode.next = currentNode.next;
+            if (currentNode.data == element) {
+                return currentIndex;
+            }
+        }
+
+        return -1;
+    }
+
+
+    public int size() {
+        int currentIndex = 0;
+        Node<T> currentNode = head;
+        while (currentNode != null) {
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+        return currentIndex;
+    }
+
+    public T get(int index) {
+        //iterate through the index
+        //return the element within that index
+        int currentIndex = 0;
+        Node<T> currentNode = head;
+        while (currentNode != null && currentIndex != index) {
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+        if (currentIndex == index && currentNode != null) {
+            return currentNode.data;
+        }
+        return null;
+    }
+
+    public SinglyLinkedList<T> copy() {
+
+        SinglyLinkedList<T> newList = new SinglyLinkedList<T>();
+        Node<T> currentNode = head;
+
+        while (currentNode != null) {
+            newList.add(currentNode.data);
+            currentNode = currentNode.next;
+        }
+
+        return newList;
+    }
+
+    public void sort() {
+        //bubble sort
+        //.compareTo() - gives you a return of 1, 0 , -1 - relative comparison
+        //iterate through the list
+        //find the largest element and move it to the end
+        //continue to iterate through the list until the values are in order from smallest to largest
+        int initialSize = size();
+        int remaining = initialSize;
+        for (int i = 0; i < initialSize; i++) {
+            int smallestIndex = 0;
+            T smallestElement = get(0);
+            for (int j = 1; j < remaining; j++) {
+                T currentElement = get(j);
+                if (currentElement.compareTo(smallestElement) < 0) {
+                    smallestElement = currentElement;
+                    smallestIndex = j;
                 }
-                // checking that the current node is the head
-                if (currentNode == head) {
-                    // and also if the current node is null
-                    if (currentNode == null || currentNode.next == null) {
-                        // return head as null;
-                        head = null;
-                    } else {
-                        // the new heas will be the next node
-                        head = currentNode.next;
-                    }
-                    // current is the tail
-                } else if (currentNode == tail) {
-                    // the tail is the previous one
-                    tail = previousNode;
-                }
             }
+            remove(smallestIndex);
+            add(smallestElement);
+            remaining--;
         }
-
-        public Boolean contains(T element) {
-            Node currentNode = head;
-            while (currentNode != null) {
-                if (currentNode != element) {
-                    if (currentNode.data != element) {
-                        currentNode = currentNode.next;
-                    } else {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-        
-        public int find(T element) {
-            int currentIndex = 0;
-            Node currentNode = head;
-            while (currentNode != null) {
-                if (currentNode.data != element) {
-                    currentNode = currentNode.next;
-                    currentIndex++;
-                }
-                if (currentNode.data == element) {
-                    return currentIndex;
-                }
-            }
-            return -1;
-        }
-
-        public int size() {
-            int currentIndex = 0;
-            Node currentNode = head;
-            while (currentNode != null) {
-                currentIndex++;
-            }
-            return currentIndex;
-        }
-
-        public T get(int index) {
-            int currentIndex = 0;
-            Node currentNode = head;
-            while (currentNode != null && currentIndex != index) {
-                currentNode = currentNode.next;
-                currentIndex++;
-            }
-            if (currentIndex == index && currentNode != null) {
-                return (T) currentNode.data;
-            }
-            return null;
-        }
-
-        public SinglyLinkedList<T> copy() {
-            SinglyLinkedList<T> newList = new SinglyLinkedList<T>();
-            Node currentNode = head;
-
-            while (currentNode != null) {
-                newList.add(currentNode.data);
-                currentNode = currentNode.next;
-            }
-            return newList;
-        }
-
-        public void sort() {
-
-        }
-
     }
 }
